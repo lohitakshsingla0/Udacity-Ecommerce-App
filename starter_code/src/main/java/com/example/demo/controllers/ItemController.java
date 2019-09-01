6c990f1f30da2fecba1a3cb2eb419518820bd05e
lohitakshsingla0 <lohitakshsingla0@gmail.com>
2019-09-02 00:41:44 +0530

    Hotfix jwt bugs

--- starter_code/src/main/java/com/example/demo/controllers/ItemController.java
+++ starter_code/src/main/java/com/example/demo/controllers/ItemController.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.persistence.Item;
 import com.example.demo.model.persistence.repositories.ItemRepository;
 
+/**
+ * The Class ItemController.
+ *
+ * @author Lohitaksh
+ */
 @RestController
 @RequestMapping("/api/item")
 public class ItemController {
